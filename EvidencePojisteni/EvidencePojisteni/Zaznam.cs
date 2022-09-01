e58aa8df3164a5e91838b76f67ac08145ce4da02
Jiří Vojtíšek Vepřek <111184853+JazzMcEve@users.noreply.github.com>
2022-09-01 13:18:42 +0200

    Add files via upload
    ep_jvv v0.9a

---
+++ EvidencePojisteni/EvidencePojisteni/Zaznam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencePojisteni
+{
+    internal class Zaznam
+    {
+        /// <summary>
+        /// konstruktor záznamu pojištěných s vlastnostmi pro zápis a čtení proměnných
+        /// </summary>
+        public string CeleJmeno { get; set; }
+        public int vek { get; set; }
+        public int telefon { get; set; }
+        public Zaznam(string celeJmeno, int vek, int telefon)
+        {
+            this.CeleJmeno = celeJmeno;
+            this.vek = vek;
+            this.telefon = telefon;
+        }
+        /// <summary>
+        /// výpis pomocí ToStringu
+        /// </summary>
+        /// <returns>vrací textový řetězec Zaznam</returns>
+        public override string ToString()
+        {
+            return CeleJmeno + "\t" + vek + "\t" + telefon;
+        }
+    }
+}
